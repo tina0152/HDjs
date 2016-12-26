cfaf140548e59e878052a2c89403e8125ef9f083
kyt <tina0152@126.com>
2016-12-26 16:07:47 +0800

    Add files via upload

---
+++ api.js
@@ -0,0 +1,98 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Ajax",
+        "Array",
+        "Browser",
+        "DeviceJump",
+        "Graphical",
+        "Loading",
+        "Number",
+        "OrientationTip",
+        "PcTips",
+        "ScreenFit",
+        "Share",
+        "SoundBtn",
+        "String",
+        "Submit"
+    ],
+    "modules": [
+        "control",
+        "data",
+        "display",
+        "media"
+    ],
+    "allModules": [
+        {
+            "displayName": "control",
+            "name": "control",
+            "description": "控制模块",
+            "classes": [
+                {
+                    "name": "Browser"
+                },
+                {
+                    "name": "DeviceJump"
+                },
+                {
+                    "name": "OrientationTip"
+                }
+            ]
+        },
+        {
+            "displayName": "data",
+            "name": "data",
+            "description": "数据模块",
+            "classes": [
+                {
+                    "name": "Ajax"
+                },
+                {
+                    "name": "Loading"
+                },
+                {
+                    "name": "ScreenFit"
+                },
+                {
+                    "name": "Share"
+                },
+                {
+                    "name": "Submit"
+                },
+                {
+                    "name": "String"
+                },
+                {
+                    "name": "Array"
+                },
+                {
+                    "name": "Number"
+                }
+            ]
+        },
+        {
+            "displayName": "display",
+            "name": "display",
+            "description": "展示模块",
+            "classes": [
+                {
+                    "name": "Graphical"
+                },
+                {
+                    "name": "PcTips"
+                }
+            ]
+        },
+        {
+            "displayName": "media",
+            "name": "media",
+            "description": "多媒体模块",
+            "classes": [
+                {
+                    "name": "SoundBtn"
+                }
+            ]
+        }
+    ]
+} };
+});
